3b0ea15a400dccd2148b2b4147755ddd05deb299
Hugo Lebeau <hugo.lebeau@ensta.fr>
2019-07-18 12:09:35 +0200

    Proper file for integratePsi functions

--- src/mixedFERegression.h
+++ src/mixedFERegression.h
@@ -8,6 +8,7 @@
 #include "param_functors.h"
 #include "regressionData.h"
 #include "solver.h"
+#include "integratePsi.h"
 #include <memory>
 
 //! A LinearSystem class: A class for the linear system construction and resolution.
@@ -123,50 +124,6 @@ public:
 		std::cout << "Option not implemented! \n";
 	}
 };
-
-template <UInt Nodes, UInt mydim, UInt ndim>
-inline Real integratePsi(const Element<Nodes,mydim,ndim>& t, UInt k)
-{
-	std::cerr<< "TRYING TO EVALUATE ORDER NOT IMPLEMENTED" << std::endl;
-	return 0;
-}
-
-template<>
-inline Real integratePsi(const Element<3,2,2>& t, UInt k)
-{
-	return t.getArea()/3;
-}
-
-template<>
-inline Real integratePsi(const Element<6,2,2>& t, UInt k)
-{
-	if (k==3 || k==4 || k==5)
-		return t.getArea()/3;
-	else
-		return 0;
-}
-
-template<>
-inline Real integratePsi(const Element<3,2,3>& t, UInt k)
-{
-	return t.getArea()/3;
-}
-
-template<>
-inline Real integratePsi(const Element<6,2,3>& t, UInt k)
-{
-	if (k==3 || k==4 || k==5)
-		return t.getArea()/3;
-	else
-		return 0;
-}
-
-template<>
-inline Real integratePsi(const Element<4,3,3>& t, UInt k)
-{
-	return t.getVolume()/4;
-}
-
 
 #include "mixedFERegression_imp.h"
 
